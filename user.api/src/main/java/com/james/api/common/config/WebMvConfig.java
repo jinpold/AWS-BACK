8bab85e26502215221ed4bd3ac5958e9be9ab727
jinpold <jinpold@gmail.com>
2024-12-18 09:41:48 +0900

    first commit

---
+++ user.api/src/main/java/com/james/api/common/config/WebMvConfig.java
@@ -0,0 +1,21 @@
+package com.james.api.common.config;
+import com.james.api.common.component.interceptor.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+    @Override
+    public void addInterceptors(@SuppressWarnings("null") InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/auth/**", "/api/news/**", "/api/articles/**", "/api/boards/**", "/api/users/**");
+
+    }
+
+}
